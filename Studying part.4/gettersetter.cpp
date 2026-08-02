fcbd807d0d048e2213b41aeb2d463dc0b5f551b6
Arbiter <trashdudx@gmail.com>
2026-08-02 21:39:10 +0700

    fix something

--- Studying part.4/gettersetter.cpp
+++ Studying part.4/gettersetter.cpp
@@ -6,17 +6,21 @@ class portion{
 
     public:
         int getportion() {
+            std::cout << "You didn't take any food?\n";
             return portion;
         }
-
         void setportion(int portion) {
             if(portion <= 0) {
-                this->portion = 0;
-                std::cout << "you haven't get any portion, here, atleast we can give your food " << portion << "portion";
+                this->portion = portion;
+                std::cout << "Why didn't take your food?\n";
+                portion = 1;
+                std::cout << "Here, " << portion << " portion should be enough";
             }
             else if(portion > 3) {
-                this->portion = 3;
-                std::cout << "Each person just can have 3 food portion, as for you " << portion << " portion is surely enough!";
+                this->portion = portion;
+                std::cout << portion << " portion!!??" << "No way, each person just can have 3 portion\n";
+                portion = 3;
+                std::cout << "So, " << portion << " should be enough for you";
             }
             else {
                 this-> portion = portion;
@@ -29,5 +33,6 @@ int main() {
     //setter = similiar with getter, but instead readable, setter make the private object attribute writeable
     portion port;
     port.getportion();
+    port.setportion(9);
     return 0;
 }
