c13546854773aaa888f33a85002ef72c155bae15
Arbiter <trashdudx@gmail.com>
2026-09-10 21:32:25 +0700

    Adding another class that calculated the slime stats

--- Bored-sketch/TamerProfile.cpp
+++ Bored-sketch/TamerProfile.cpp
@@ -8,29 +8,33 @@ class Equipment {
     std::string sword;
     std::string accessory;
     std::string armor;
+    int Atk;
+    int energy;
+    int spd;
+    int hp;
 };
 
 class Sword : public Equipment {
     public:
         void stick() {
-            int Atk = 9;
-            int deffense = 0;
-            int spd = 5;
-            int hp = 10;
+            Atk = 9;
+            energy = 0;
+            spd = 5;
+            hp = 10;
         }
 
         void sword() {
-            int Atk = 15;
-            int deffense = 0;
-            int spd = 3;
-            int hp = 13;
+            Atk = 15;
+            energy = 0;
+            spd = 3;
+            hp = 13;
         }
 
         void greatsword() {
-          int Atk = 20;
-            int deffense = 0;
-            int spd = -3;
-            int hp = 15;
+          Atk = 20;
+            energy = 0;
+            spd = -3;
+            hp = 15;
         };
 
 };
@@ -38,47 +42,47 @@ class Sword : public Equipment {
 class Armor : public Equipment {
     public:
         void iron() {
-            int Atk = 3;
-            int deffense = 20;
-            int spd = 0;
-            int hp = 30;
+            Atk = 3;
+            energy = 20;
+            spd = 0;
+            hp = 30;
         }
 
         void diamond() {
-            int Atk = 3;
-            int deffense = 30;
-            int spd = 0;
-            int hp = 50;
+            Atk = 3;
+            energy = 30;
+            spd = 0;
+            hp = 50;
         }
 
         void chain() {
-            int Atk = 0;
-            int deffense = 10;
-            int spd = 3;
-            int hp = 20;
+            Atk = 0;
+            energy = 10;
+            spd = 3;
+            hp = 20;
         }
 };
 
 class Accessory : public Equipment {
     public:
         void necklace() {
-            int Atk = 5;
-            int deffense = 7;
-            int spd = 3;
-            int hp = 2;
+            Atk = 5;
+            energy = 7;
+            spd = 3;
+            hp = 2;
         }
 
         void boots() {
-            int Atk = 1;
-            int deffense = 9;
-            int spd = 5;
-            int hp = 5;
+            Atk = 1;
+            energy = 9;
+            spd = 5;
+            hp = 5;
         }
         void hat() {
-            int Atk = 3;
-            int deffense = 6;
-            int spd = 2;
-            int hp = 7;
+            Atk = 3;
+            energy = 6;
+            spd = 2;
+            hp = 7;
         }
 };
 class Pet{
@@ -96,15 +100,11 @@ class Pet{
 namespace {
     class Companion : public Pet {
     public:
-        void Direwolf(int HP, int energy, int damage, int speed) {
-            this->HP = HP;
-            HP = 100;
-            this->energy = energy;
-            energy = 10;
-            this->damage = damage;
-            damage = 2;
-            this->speed = speed;
-            speed = 5;
+        void Direwolf() {
+            HP = 120;
+            energy = 12;
+            damage = 10;
+            speed = 12;
 
             std::cout << "Species: Direwolf\n";
             std::cout << "HP: " << HP << '\n';
@@ -113,15 +113,11 @@ namespace {
             std::cout << "Speed: " << speed << '\n';
         }
 
-        void Griffin(int HP, int energy, int damage, int speed) {
-            this->HP = HP;
-            HP = 120;
-            this->energy = energy;
+        void Griffin() {
+            HP = 100;
             energy = 10;
-            this->damage = damage;
-            damage = 5;
-            this->speed = speed;
-            speed = 10;
+            damage = 15;
+            speed = 17;
 
             std::cout << "Species: Griffin\n";
             std::cout << "HP: " << HP << '\n';
@@ -130,15 +126,11 @@ namespace {
             std::cout << "Speed: " << speed << '\n';
         }
 
-        void Slime(int HP, int energy, int damage, int speed) {
-            this->HP = HP;
+        void Slime() {
             HP = 70;
-            this->energy = energy;
-            energy = 2;
-            this->damage = damage;
-            damage = 1;
-            this->speed = speed;
-            speed = 2;
+            energy = 5;
+            damage = 7;
+            speed = 5;
 
             std::cout << "Species: Slime\n";
             std::cout << "HP: " << HP << '\n';
@@ -151,15 +143,11 @@ namespace {
 namespace {
     class Attacker : public Pet {
     public:
-        void Fenrir(int HP, int energy, int damage, int speed) {
-            this->HP = HP;
-            HP = 300;
-            this->energy = energy;
-            energy = 30;
-            this->damage = damage;
-            damage = 10;
-            this->speed = speed;
-            speed = 12;
+        void Fenrir() {
+            HP = 200;
+            energy = 25;
+            damage = 20;
+            speed = 21;
 
             std::cout << "Species: Fenrir\n";
             std::cout << "HP: " << HP << '\n';
@@ -171,14 +159,10 @@ namespace {
             }
         }
 
-        void Pheonix(int HP, int energy, int damage, int speed) {
-            this->HP = HP;
-            HP = 235;
-            this->energy = energy;
-            energy = 24;
-            this->damage = damage;
-            damage = 15;
-            this->speed = speed;
+        void Phoenix() {
+            HP = 190;
+            energy = 30;
+            damage = 19;
             speed = 30;
 
             std::cout << "Species: Phoenix\n";
@@ -191,15 +175,11 @@ namespace {
             }
         }
 
-        void MysticHound(int HP, int energy, int damage, int speed) {
-            this->HP = HP;
-            HP = 240;
-            this->energy = energy;
-            energy = 30;
-            this->damage = damage;
-            damage = 18;
-            this->speed = speed;
-            speed = 10;
+        void MysticHound() {
+            HP = 195;
+            energy = 25;
+            damage = 17;
+            speed = 22;
 
             std::cout << "Species: Mystic Hound\n";
             std::cout << "HP: " << HP << '\n';
@@ -207,7 +187,7 @@ namespace {
             std::cout << "Damage: " << damage << '\n';
             std::cout << "Speed: " << speed << '\n';
             if(equip.equipment == "-") {
-                std::cout << "Your beast hasn't wear any equpment yet";
+                std::cout << "Your beast hasn't wear any equipment yet";
             }
         }
     };
@@ -279,8 +259,35 @@ class companionStat{
         Companion statComp;
         Sword statSword;
 
-    void slimeStat() {
-        
+    void slimeStat1() {
+        statComp.Slime();
+        statSword.stick();
+
+        std::cout << "Species: Slime\n";
+        std::cout << "HP: " << statComp.HP + statSword.hp << '\n';
+        std::cout << "Energy: " << statComp.energy + statSword.energy << '\n';
+        std::cout << "Damage: " << statComp.damage + statSword.Atk << '\n';
+        std::cout << "Speed: " << statComp.speed + statSword.spd << '\n';
+    }
+    void slimeStat2() {
+        statComp.Slime();
+        statSword.sword();
+
+        std::cout << "Species: Slime\n";
+        std::cout << "HP: " << statComp.HP + statSword.hp << '\n';
+        std::cout << "Energy: " << statComp.energy + statSword.energy << '\n';
+        std::cout << "Damage: " << statComp.damage + statSword.Atk << '\n';
+        std::cout << "Speed: " << statComp.speed + statSword.spd << '\n';
+    }
+    void slimeStat3() {
+        statComp.Slime();
+        statSword.greatsword();
+
+        std::cout << "Species: Slime\n";
+        std::cout << "HP: " << statComp.HP + statSword.hp << '\n';
+        std::cout << "Energy: " << statComp.energy + statSword.energy << '\n';
+        std::cout << "Damage: " << statComp.damage + statSword.Atk << '\n';
+        std::cout << "Speed: " << statComp.speed + statSword.spd << '\n';
     }
 };
 int main() {
@@ -292,6 +299,7 @@ int main() {
     Attacker attack;
     tool tool;
     Equipment equip;
+    companionStat stats;
     
 
     loading();
@@ -327,7 +335,7 @@ int main() {
                    }
                    switch(tool.choose) {
                        case 1:
-                           compa.Direwolf(pet.HP, pet.energy, pet.damage, pet.speed);
+                           compa.Direwolf();
                            std::cout << "Would you like to change something?(y/n): ";
                            std::cin >> tool.decide;
                            tool.decide = tolower(tool.decide);
@@ -338,7 +346,7 @@ int main() {
                            }
                            break;
                        case 2:
-                           compa.Griffin(pet.HP, pet.energy, pet.damage, pet.speed);
+                           compa.Griffin();
                            std::cout << "Would you like to change something?(y/n): ";
                            std::cin >> tool.decide;
                            tool.decide = tolower(tool.decide);
@@ -351,7 +359,7 @@ int main() {
                            }
                            break;
                        case 3:
-                           compa.Slime(pet.HP, pet.energy, pet.damage, pet.speed);
+                           compa.Slime();
                            std::cout << "Would you like to change something?(y/n): ";
                            std::cin >> tool.decide;
                            tool.decide = tolower(tool.decide);
@@ -377,7 +385,21 @@ int main() {
                                                Sleep(100);
                                                std::cout << c;
                                            }
-
+                                           stats.slimeStat1();
+                                       } else if (tool.choice == 2) {
+                                           std::string text = "Loading..\n";
+                                           for (char c : text) {
+                                               Sleep(100);
+                                               std::cout << c;
+                                           }
+                                           stats.slimeStat2();
+                                       } else if (tool.choice == 3) {
+                                           std::string text = "Loading..\n";
+                                           for (char c : text) {
+                                               Sleep(100);
+                                               std::cout << c;
+                                           }
+                                           stats.slimeStat3();
                                        }
                                        break;
                                }
@@ -407,13 +429,13 @@ int main() {
         std::cin >> tool.choose;
         switch(tool.choose) {
             case 1:
-                attack.Fenrir(pet.HP, pet.energy, pet.damage, pet.speed);
+                attack.Fenrir();
                 break;
             case 2:
-                attack.Pheonix(pet.HP, pet.energy, pet.damage, pet.speed);
+                attack.Phoenix();
                 break;
             case 3:
-                attack.MysticHound(pet.HP, pet.energy, pet.damage, pet.speed);
+                attack.MysticHound();
                 break;
             default:
                     std::cout << "You hasn't tame that species yet\n";
